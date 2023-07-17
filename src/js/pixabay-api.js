19c1b35223263addd50f88b7dab3ee7aef16f917
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-07-17 09:48:44 +0300

    change

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,49 @@
+// import axios from 'axios';
+// export class PixabayAPI {
+//   #BASE_URL = 'https://pixabay.com/api/';
+//   #API_KEY = '38183067-20cd9da803a0e7530f454b294';
+//   #BASE_SEARCH_PARAMS = {
+//     key: this.#API_KEY,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   };
+
+//   page = 1;
+//   q = null;
+//   per_page = 40;
+
+//   async fetchImgs() {
+//     const searchParams = new URLSearchParams({
+//       ...this.#BASE_SEARCH_PARAMS,
+//       page: this.page,
+//       q: this.q,
+//       per_page: this.per_page,
+//     });
+//     return await axios.get(`${this.#BASE_URL}?${searchParams}`);
+//   }
+// }
+
+
 import axios from 'axios';
 
 export class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '38183067-20cd9da803a0e7530f454b294';
   #BASE_SEARCH_PARAMS = {
-    key: this.#API_KEY,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   };
 
-  page = 1;
-  q = null;
-  per_page = 40;
+  constructor() {
+    this.page = 1;
+    this.q = null;
+    this.per_page = 40;
+
+    // Adding the API key to the search parameters
+    this.#BASE_SEARCH_PARAMS.key = this.#API_KEY;
+  }
 
   async fetchImgs() {
     const searchParams = new URLSearchParams({
@@ -21,6 +52,6 @@ export class PixabayAPI {
       q: this.q,
       per_page: this.per_page,
     });
-    return await axios.get(`${this.#BASE_URL}?${this.#BASE_SEARCH_PARAMS}`);
+    return await axios.get(`${this.#BASE_URL}?${searchParams}`);
   }
 }
